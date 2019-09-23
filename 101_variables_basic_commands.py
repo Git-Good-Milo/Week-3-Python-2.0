3d5923ef16c3736f353c831552395307a2f2c4bc
Miles Eastwood <MEastwood@spartaglobal.com>
2019-09-23 10:39:14 +0100

    Looked at variables, print and type functions

---
+++ 101_variables_basic_commands.py
@@ -0,0 +1,36 @@
+# Variables in Python
+    # A variable is a box. You can give it a name and out stuff inside
+
+# Assigning a variable
+# Giving the box a name and putting stuff inside
+box_variable = 'Books and Stuff'
+
+# Looking inside the box
+print(box_variable)
+
+#Re assigning a varible
+# Variables are mutables, hence they can changed / be re-assigned
+
+box_variable = "Cd's and other stuff"
+print(box_variable)
+
+#Re-assigning with an Integer
+# A variable can hold any type of data type inside
+box_variable = 14
+print(box_variable)
+
+# Important python function
+# print(arg)
+
+print('Hello') # Printing string
+print(box_variable) # Printing Variable
+print('Hello', box_variable, 5*5, 'etc') #Overloading with two arguments
+
+# type(arg)
+# output the data type of an object
+print(type('Hello'))
+print(type(14))
+print(type(14.0))
+
+variable_num = '14'
+#print(type('10'*variable_num))  # This will break
