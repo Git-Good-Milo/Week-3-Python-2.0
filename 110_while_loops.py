1eeb09673f5f85ce236c31f843bec6c13d3e1b2d
Miles Eastwood <MEastwood@spartaglobal.com>
2019-09-24 15:18:52 +0100

    Started for loops, if statements and while loops

---
+++ 110_while_loops.py
@@ -0,0 +1,18 @@
+# While loops
+
+# keeps looping and iteration until a condition is met
+# Or it comes into a break statement
+
+# Syntax 1
+# while <condition>:
+    # block
+    # if statement
+        # if <condition>:
+            # break
+import time
+counter = 0
+while counter < 10:
+    print(counter)
+    print ('Pewwwwwwdipie')
+    counter += 1
+    time.sleep(1)
